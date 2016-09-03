4c1928eef150e6acb240254aceaa8646e2c60987
Innoviox <sarras305@gmail.com>
2016-09-03 17:20:35 -0400

    Update scrabble.py

--- scrabble.py
+++ scrabble.py
@@ -31,6 +31,13 @@ root.geometry("%dx%d%+d%+d" % (300, 300, 0, 0))
 global playing #To tell if the player is currently playing; for the save game function
 playing = 0
 
+#Initialize subdictionaries for CPU
+try:
+  open("resources/AI.txt")
+except:
+  print("Initializing subdictionaries")
+  import diphthin
+  
 #Tiles in "bag"
 global distribution
 #distribution = ["a", "a", "a", "a", "a", "a", "a", "a", "a", "b", "b", "c", "c", "d", "d", "d", "d", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "f", "f", "g", "g", "g", "h", "h", "i", "i", "i", "i", "i", "i", "i", "i", "i", "j", "k", "l", "l", "l", "l", "m", "m", "n", "n", "n", "n", "n", "n", "o", "o", "o", "o", "o", "o", "o", "o", "p", "p", "q", "r", "r", "r", "r", "r", "r","s", "s", "s", "s", "t", "t", "t", "t", "t", "t", "u", "u", "u", "u", "v", "v", "w", "w", "x", "y", "y", "z", "?", "?"]
