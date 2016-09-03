99f3a39c7693219acb244ba32a91d8b48c53b636
Innoviox <sarras305@gmail.com>
2016-09-03 17:22:07 -0400

    Update diphthin.py

--- diphthin.py
+++ diphthin.py
@@ -1,3 +1,5 @@
+#Makes subdictionaries for each diphthong (group of two letters), which makes checking a word hundreds of times faster.
+
 ospd = open("newDict.txt").read().split("\n") #taken from https://raw.githubusercontent.com/xjtian/PyScrabble/master/wordlists/OSPD4_stripped.txt
 nospd = []
 for word in ospd:
@@ -9,9 +11,9 @@ diphths = ["".join(i) for i in p(list(a_u), 2)]
 for i in a_u:
     diphths.append(i*2)
 for i in diphths:
-    with open(i+".txt", "w"):
+    with open("resources/" + i+".txt", "w"):
         pass
-    dfile = open(i+".txt", "w")
+    dfile = open("resources/" + i+".txt", "w")
     for word in nospd:
         #print(word[:2])
         if word[:2] == i:
