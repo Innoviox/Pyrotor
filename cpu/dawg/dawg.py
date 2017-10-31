6ff5a2ec95f220a4585a6093b25c28b504751c7e
Innoviox <sarras305@gmail.com>
2017-10-30 20:14:08 -0400

    done w/comments

--- cpu/dawg/dawg.py
+++ cpu/dawg/dawg.py
@@ -1,3 +1,5 @@
+def inDict(item, d):
+    return item in d.keys() or item in d.values()
 class Node:
     _id = 0
     def __init__(self, prev, end=False):
@@ -7,8 +9,9 @@ class Node:
         self.end = end
         self.actions = {}
         
-    def addAction(self, action, nodeTo):
-        if action not in self.actions:
+    def addAction(self, action, nodeTo, force=False):
+        #print("\t\t\tadding action", action, nodeTo)
+        if force or action not in self.actions:
             self.actions[action] = nodeTo
 
     def getactions(self):
@@ -61,11 +64,11 @@ class Node:
     def catalysts(self):
         if self.prev is not None:
             return list(self.prev.actionlead(self))
-        return None
+        return [None]
 
     def actionlead(self, node):
         for action, nodeTo in self.actions.items():
-            if nodeTo is node:
+            if nodeTo.id == node.id:
                 yield action
                 
     def coolstr(self):
@@ -137,7 +140,7 @@ class Dawg(Trie):
     def add(self, word):
         print("adding", word)
         super(Dawg, self).add(word)
-        self.minimize()
+        #self.minimize()
         
         
     def minimize(self):
@@ -145,72 +148,98 @@ class Dawg(Trie):
         cantdelete=[]
         for node in self.nodes:
             for node2 in self.nodes:
-                if node2 not in deleted and node2 not in node.path():
-                    #print("Cycling", node, node2, "\n", sep="\n\t")
-                
-                    for action, nodeTo in node.actions.copy().items():
-                        if action in node2.actions and action != "!":
-                            p=node.prev
-                            #print(p)
-                            #print("\tCycling", node, node2, "\n", sep="\n\t")
-                            if p.catalysts() == node2.prev.catalysts():
-                                for c_action in node2.catalysts():
-                                    #print("\tpreadding", c_action)
-                                    
-                                        p.actions[c_action] = node
-                            else:
-                                pass
-                                #print("\t\tpreadd stopped", p.catalysts(), node2.catalysts())
-                            #print(p)
-                            for n_action, nodeTo in node2.actions.items():
-                                #print("a\t", node, node2, nodeTo, n_action, sep="\n\t")
-                                #print("\tpostadding", n_action, nodeTo)
-                                if node.prev.catalysts() == nodeTo.prev.catalysts():
-                                    if n_action not in node.actions:
-                                        #print("\t\tpoa2")
-                                        node.addAction(n_action, nodeTo)
-                                    else:
-                                        _node = node.actions[n_action]
-                                        for _n_action, _nodeTo in nodeTo.actions.items():
-                                            #print("\t\t\tpoa3", _n_action, _nodeTo)
-                                            _node.addAction(_n_action, _nodeTo)
-                            if node2 not in cantdelete:
-                                deleted.append(node2)
-                                cantdelete.extend(node2.path())
-                                cantdelete.append(node2)
-
+                if node.id != node2.id:
+                    if node2 not in deleted and node2 not in node.path():
+                        #print("Cycling", node, node2, "\n", sep="\n\t")
+                        for action, nodeTo in node.actions.copy().items():
+                            if action in node2.actions and action != "!":
+                                p=node.prev
+                                #print(p)
+                                #print("\tCycling", node, node2, "\n", sep="\n\t")
+                                if p.catalysts() == node2.prev.catalysts():
+                                    for c_action in node2.catalysts():
+                                        #print("\tpreadding", c_action)
+                                        p.addAction(c_action, node, force=True)
+                                else:
+                                    pass
+                                    #print("\t\tpreadd stopped", p.catalysts(), node2.prev.catalysts())
+                                for n_action, nodeTo in node2.actions.items():
+                                    #print("a\t", node, node2, nodeTo, n_action, sep="\n\t")
+                                    #print("\tpostadding", n_action, nodeTo)
+                                    if node.prev.catalysts() == nodeTo.prev.catalysts():
+                                        if n_action not in node.actions:
+                                            #print("\t\tpoa2")
+                                            node.addAction(n_action, nodeTo)
+                                        else:
+                                            _node = node.actions[n_action]
+                                            for _n_action, _nodeTo in nodeTo.actions.items():
+                                                #print("\t\t\tpoa3", _n_action, _nodeTo)
+                                                _node.addAction(_n_action, _nodeTo)
+                                if node2 not in cantdelete:
+                                    deleted.append(node2)
+                                    cantdelete.extend(node2.path())
+                                    cantdelete.append(node2)
+##        oldnodes = self.nodes[:]
+##        while oldnodes != self.nodes:
+##            oldnodes = self.nodes[:]
+##            for node in self.nodes:
+##                for node2 in self.nodes:
+##                    if node.id != node2.id:
+                    #if not inDict(node, self.replacements) and not inDict(node2, self.replacements):
                     if not node.onlyEnd() and node.ame() == node2.ame():
-                        
                         same = True
                         for ((action1, nodet1), (action2, nodet2)) in zip(node.actions.copy().items(), node2.actions.copy().items()):
                             if nodet1.ame() != nodet2.ame():
                                 same = False
                         if same:
-                            p=node.prev
+                            p2=node2.prev
                             for c_action in node2.catalysts():
-                                p.actions[c_action] = node
-                            if node not in self.replacements.keys() and node not in self.replacements.values():
-                               self.replacements[node2] = node
+                                if c_action is not None:
+                                    p2.addAction(c_action, node, force=True)
+                            for action, nodeTo in node2.actions.items():
+                                if action in node.actions:
+                                    nodeTo.prev = node
+                                    node.addAction(action, nodeTo, force=True)
+                            self.replacements[node2] = node
+                            #print("merging", node, node2)
+                            #print("\t", node.coolstr(), node2.coolstr(), sep="\n\t")
+            
         for node in self.nodes:
             node.update(self.replacements)
-            
+
         for node in deleted:
+            continue
+            print('deleting', node.coolstr(), sep='\n')
             self.nodes.remove(node)
             self.deletednodes.append(node)
 
-lex = ["CAR", "CARS", "CAT", "CATS","DO", "DOG", "DOGS", "DONE", "EAR", "EARS", "EAT", "EATS"]                           
-t = Dawg()
-for w in lex:
-    t.add(w)
-    #input(t)
-    #print(t)
-#print(t.acceptable("EAT"))
-for n in t.deletednodes:
-    t.nodes.append(n)
-print(t)
-for w in lex:
-    print(t.acceptable(w))
-print(t.acceptable("EO"))
-print(t.parse("CA"))
-print(t.parse("DO"))
-print(t.start.actions.keys())
+if __name__ == "__main__":
+    lex = ["CAR", "CARS", "CAT", "CATS", "DO", "DOG", "DOGS", "DONE", "EAR", "EARS", "EAT", "EATS"]                           
+    t = Dawg()
+    for w in lex:
+        t.add(w)
+        #\t.minimize()
+        #input(t)
+        #print(t)
+    #print(t.acceptable("EAT"))
+    print(list(filter(lambda i:i.id==47, t.nodes))[0].getactions())
+    #for i in range(len(t.nodes)):
+    for i in range(1):
+        t.minimize()
+        #input(t)
+    for n in t.deletednodes:
+        pass
+        #t.nodes.append(n)
+    print(t)
+    #print(t.replacements)
+    for w in lex:
+        print(w, t.parse(w))
+    #print(list(filter(lambda i:i.id==47, t.nodes))[0].getactions())
+    ##print(t.parse("CAR"))
+    ##print(t.parse("DOG"))
+
+    ##import random
+    ##s = "ACDEGORST"
+    ##for i in range(100):
+    ##    q=''.join(random.sample(s, random.choice([3, 4])))
+    ##    print(q, t.acceptable(q))
